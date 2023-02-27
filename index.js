0a59a8e88c761a9e093c31f9df1051da7a6545de
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-02-27 19:40:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const fs = require('fs')
+const http = require('http')
+const url = require('url')
 
 // //syncronous blocking way
 // const textIn =fs.readFileSync('./ola.txt', 'utf-8')
@@ -9,16 +11,25 @@ const fs = require('fs')
 // const h = fs.readFileSync('./jok.txt', 'utf-8');
 // console.log(h)
 
-//asycronous non-blocking way
-fs.readFile('./krypthonverse.txt', 'utf-8',(error, data)=>{
-    fs.readFile(`./${data}.txt`, 'utf-8',(error, data1)=>{
-        console.log(data1)
-        fs.readFile('./jok.txt', 'utf-8',(error, data2)=> {
-            console.log(data2)
-            fs.writeFile('./kry.txt', `${data1}\n${data1}`,'utf-8', err =>{
-                console.log('file has been writen ...')
-            })
-        })
-    })
+// asycronous non-blocking way
+// fs.readFile('./krypthonverse.txt', 'utf-8',(error, data)=>{
+//     fs.readFile(`./${data}.txt`, 'utf-8',(error, data1)=>{
+//         console.log(data1)
+//         fs.readFile('./jok.txt', 'utf-8',(error, data2)=> {
+//             console.log(data2)
+//             fs.writeFile('./kry.txt', `${data1}\n${data1}`,'utf-8', err =>{
+//                 console.log('file has been writen ...')
+//             })
+//         })
+//     })
 
+// })
+
+const server= http.createServer((req, res)=>{
+    res.end('Hellow world Raimundo')
+    console.log(res)
+})
+
+server.listen(8080, 'localhost',(req, res)=>{
+    console.log("server running ")
 })
