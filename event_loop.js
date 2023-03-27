ad1f4003bc3423b3e7b579bd5194867da3dfa705
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-03-27 20:19:26 -0300

    Update event_loop.js

--- event_loop.js
+++ event_loop.js
@@ -6,5 +6,8 @@ setImmediate(()=> console.log('immediate 1 finished'),0)
 fs.readFile('test-file.txt',()=>{
     console.log('I/O finished')
 })
+setTimeout(() => console.log('timed 2 finished'), 0)
+setTimeout(() => console.log('timed 3 finished'), 3000)
+setImmediate(() => console.log('immediate 2 finished'))
 
 console.log('hello from top level code')
