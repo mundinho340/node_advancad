0cd28429068c09f148935a7897ce6f692056144d
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-03-28 12:48:35 -0300

    Update event_loop.js

--- event_loop.js
+++ event_loop.js
@@ -1,13 +1,17 @@
 const fs = require('fs')
+const crypto = require('crypto')
 
 setTimeout(()=> console.log('timed 1 fineshed'), 0)
 setImmediate(()=> console.log('immediate 1 finished'),0)
 
 fs.readFile('test-file.txt',()=>{
     console.log('I/O finished')
+    console.log('---------------------------------')
+    setTimeout(() => console.log('timed 2 finished'), 0)
+    setTimeout(() => console.log('timed 3 finished'), 3000)
+    setImmediate(() => console.log('immediate 2 finished'))
+    process.nextTick(() => console.log('process.Next tick'))
+    crypto.pbkdf2()
 })
-setTimeout(() => console.log('timed 2 finished'), 0)
-setTimeout(() => console.log('timed 3 finished'), 3000)
-setImmediate(() => console.log('immediate 2 finished'))
 
 console.log('hello from top level code')
