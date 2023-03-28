52950030b2278a7d9862b5e522a6d2535d0d2fac
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-03-28 14:16:36 -0300

    ppop

--- event_loop.js
+++ event_loop.js
@@ -7,11 +7,11 @@ setImmediate(()=> console.log('immediate 1 finished'),0)
 fs.readFile('test-file.txt',()=>{
     console.log('I/O finished')
     console.log('---------------------------------')
-    setTimeout(() => console.log('timed 2 finished'), 0)
-    setTimeout(() => console.log('timed 3 finished'), 3000)
-    setImmediate(() => console.log('immediate 2 finished'))
-    process.nextTick(() => console.log('process.Next tick'))
-    crypto.pbkdf2()
+    setTimeout(() => console.log('timed 2 finished'), 0);
+    setTimeout(() => console.log('timed 3 finished'), 3000);
+    setImmediate(() => console.log('immediate 2 finished'));
+    process.nextTick(() => console.log('process.Next tick'));
+    crypto.pbkdf2('password', 'salt', 100000,1024,'sha512');
 })
 
 console.log('hello from top level code')
