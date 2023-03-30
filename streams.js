b32586d21bebe7ed5b3448bf28441421852dd9c9
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-03-30 18:34:10 -0300

    Update streams.js

--- streams.js
+++ streams.js
@@ -1,7 +1,8 @@
 // No node existem 4 tipos de stream que são:
 /*
 Description                 Example            Important Event          Important Function
-Readable streams  ->
+Readable streams  ->        https requests      data                        pipe()
+                            fs read streams     end                         read()
 Writable streams -> 
 Duplex Streams ->
 Tranform Streams ->*/;
